ef6ad18f3b12c94af8a52544d8618105de51f898
rayanaxoz <rauanadaurbekova0.1@gmail.com>
2025-10-23 21:31:01 +0300

    feat: add personalized greeting feature

--- src/main/java/com/student/helloworld/Main.java
+++ src/main/java/com/student/helloworld/Main.java
@@ -1,7 +1,20 @@
 package com.student.helloworld;
 
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name + "!");
+        scanner.close();
+    }
+}package com.student.helloworld;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
 }
+
